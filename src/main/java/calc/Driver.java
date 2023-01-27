75af20f1c4d597eb42ac6d6e7c412d864e3bfba1
aravind-dasarat <aravind.dasarat@gmail.com>
2023-01-27 02:48:31 -0500

    swing code for calc app

---
+++ src/main/java/calc/Driver.java
@@ -0,0 +1,14 @@
+package calc;
+
+/**
+ * @author Aravind Dasarathy
+ */
+public class Driver {
+    public static void main(String[] args) {
+        System.out.println("============Main Execution Start===================\n\n");
+
+        Calculator.demo();
+
+        System.out.println("\n\n============Main Execution End===================");
+    }
+}
